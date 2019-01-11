4cd7b73df92ed7b12d45008af02a7047a2b29f95
Valentin Boettcher <valentin@boettcher.cf>
2019-01-11 11:03:46 +0100

    Add footnote jumping

--- js/main.js
+++ js/main.js
@@ -108,15 +108,59 @@ function setUpNumbering() {
     }
 };
 
+function collectFootnotes() {
+    let footMap = [];
+
+    // collect al footnotes into pairs
+    for(let note of document.querySelectorAll('sup')) {
+	let index = parseInt(note.innerText) - 1;
+
+	// JS forces me to do that!
+	if(footMap[index])
+	    footMap[index].push(note);
+	else
+	    footMap[index] = [ note ];
+    }
+
+    return footMap;
+}
+
+function linkFootnotes() {
+    function identifyAndLink(index, originid, targetid, origin, target) {
+	origin.id = `foot${index}${originid}`;
+	let link = document.createElement('a');
+	target.id = `foot${index}${targetid}`;
+	link.href = '#' + target.id;
+	link.innerText = ++index;
+	origin.innerHTML = '';
+	origin.appendChild(link);
+    };
+    
+    let footMap = collectFootnotes();
+
+    for(let index in footMap) {
+	let pair = footMap[index];
+	if(pair && pair.length == 2) {
+	    let first = pair[0];
+	    let second = pair[1];
+
+	    identifyAndLink(index, 1, 2, first, second);
+	    identifyAndLink(index, 2, 1, second, first);
+	}	    
+    }
+}
+
 // Let's apply that stuff.
 loadChapters().then(newChapters => {
     chapters = newChapters;
     setUpNumbering();
 });
 
+linkFootnotes();
 wrapPre();
 HighlightLisp.highlight_auto();
 HighlightLisp.paren_match();
 createTOC();
 setUpNav();
+
 
